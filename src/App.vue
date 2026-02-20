80331ff7a672da93384c43f07c53acbbab06c255
Saif Khan <saif@radix.email>
2026-02-20 15:50:37 +0530

    Added tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-<script setup></script>
-
 <template>
+  <div>
   <h1>You did it!</h1>
+  <h1 class="text-3xl font-bold underline bg-sky-500">Hello world!</h1>
+
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
-</template>
 
-<style scoped></style>
+  </div>
+</template>
