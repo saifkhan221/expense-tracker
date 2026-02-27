23266622db86bf9500f4e469f377ccc1f0183af7
Saif Khan <saif@radix.email>
2026-02-27 11:34:15 +0530

    Added design system colors

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
-  <h1>You did it!</h1>
-  <h1 class="text-3xl font-bold underline bg-sky-500">Hello world!</h1>
-
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1 class="text-red-500">Expense Tracker</h1>
+  <h2 class="text-3xl font-bold underline bg-sky-500">Track Your Expenses!</h2>
 
   </div>
 </template>
